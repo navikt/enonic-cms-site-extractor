73dcac758a67a13f35ac1eaba044f9d11ab37190
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-01-12 21:16:51 +0100

    Fjerner call logging

--- src/main/kotlin/no/nav/Application.kt
+++ src/main/kotlin/no/nav/Application.kt
@@ -1,22 +1,15 @@
 package no.nav
 
 import io.ktor.server.application.*
-import io.ktor.server.plugins.callloging.*
-import io.ktor.server.request.*
 import io.ktor.server.resources.*
 import io.ktor.server.routing.*
 import no.nav.routing.configureRouting
-import org.slf4j.event.Level
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     install(IgnoreTrailingSlash)
-
-    install(CallLogging) {
-        filter { call -> !call.request.path().startsWith("/internal") }
-    }
 
     install(Resources)
 
